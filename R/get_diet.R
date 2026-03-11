d685564f1acdd9ff1253c5d46d401b5c67aa47f5
ismael-lajaaiti <ismael.lajaaiti@gmail.com>
2026-03-11 16:11:28 +0100

    fishbase diet data

---
+++ R/get_diet.R
@@ -0,0 +1,63 @@
+#' Get fish diet with standardized categories.
+#'
+#' @param species A character vector of species names.
+#' @return A data frame with species name ("species"), predator stage ("stage") and prey category ("prey_category").
+#' @import dplyr
+#' @export
+get_diet_category <- function(species) {
+    items <- rfishbase::fb_tbl("diet_items")
+    diet_species <- rfishbase::diet(species)
+    diet_category <- diet_species |>
+        left_join(items, by = "DietCode") |>
+        select(Species, SampleStage, DietCode, FoodI, FoodII, FoodIII, ItemName, Stage, DietPercent) |>
+        filter(DietPercent > 10) |>
+        rename(PredatorStage = SampleStage, PreyStage = Stage) |>
+        mutate(
+            prey_category = case_when(
+                FoodII == "phytoplankton" ~ "phytoplankton",
+                FoodII == "other plants" & tolower(ItemName) == "macrophyte" ~ "macrophyte",
+                FoodII == "other plants" & FoodIII == "terrestrial plants" ~ "macrophyte",
+                FoodII == "other plants" & grepl("macrophyte", tolower(ItemName)) ~ "macrophyte",
+                FoodII == "other plants" & grepl("algae", tolower(FoodIII)) ~ "biofilm",
+                FoodI == "nekton" ~ "fish",
+                TRUE ~ FoodI
+            )
+        ) |>
+        select(Species, PredatorStage, prey_category) |>
+        distinct(Species, PredatorStage, prey_category) |>
+        arrange(Species, desc(PredatorStage)) |>
+        rename(species = Species, stage = PredatorStage)
+    return(diet_category)
+}
+
+#' Widen the dataframe of species diets.
+#'
+#' @param diet_category Output of get_diet_category.
+#' @return Same as get_diet_categery, but in wide format.
+#' @import dplyr
+#' @importFrom tidyr pivot_wider
+#' @export
+widen_diet_category <- function(diet_category) {
+    diet_category_wide <- diet_category |>
+        mutate(eat = 1) |>
+        pivot_wider(
+            names_from = prey_category,
+            values_from = eat,
+            values_fill = list(eat = 0)
+        ) |>
+        select(-others) |>
+        arrange(species, desc(stage))
+    return(diet_category_wide)
+}
+
+#' Get fish diet with standardized categories in a wide format.
+#'
+#' @param species List of species names.
+#' @return Same as get_diet_categery, but in wide format.
+#' @export
+get_diet_category_wide <- function(species) {
+    diet_category_wide <- species |>
+        get_diet_category() |>
+        widen_diet_category()
+    return(diet_category_wide)
+}
--- R/utils.R
+++
@@ -1,4 +0,0 @@
-#' Add two numbers
-#'
-#' @export
-add <- function(x, y) x + y
