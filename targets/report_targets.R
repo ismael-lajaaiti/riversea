a671df5698ca2d90b0e97a69d49f2092d76077f4
ismael-lajaaiti <ismael.lajaaiti@gmail.com>
2026-04-16 14:59:24 +0200

    Update ms and report B3

--- targets/report_targets.R
+++ targets/report_targets.R
@@ -5,8 +5,7 @@ report_targets <- list(
     "index.qmd",
     quarto_args = c("--embed-resources")
   ),
-  tar_quarto(
-    report_A1,
+  tar_quarto(report_A1,
     "analysis/A1-model-eel-abundance.qmd",
     quarto_args = c("--embed-resources")
   ),
@@ -18,17 +17,18 @@ report_targets <- list(
   tar_quarto(
     report_B2,
     "analysis/B2-get-diet-fishbase.qmd",
-    quarto_args = c("--embed-resources"),
+    quarto_args = c("--embed-resources")
   ),
   tar_quarto(
     report_B3,
     "analysis/B3-build-food-webs.qmd",
     quarto_args = c("--embed-resources"),
+    quiet = FALSE
   ),
   tar_quarto(
     report_C1,
     "analysis/C1-explore-coast-data.qmd",
-    quarto_args = c("--embed-resources"),
+    quarto_args = c("--embed-resources")
   ),
   tar_quarto(
     report_C2,
@@ -44,7 +44,7 @@ report_targets <- list(
   tar_quarto(
     presentation_wine,
     "presentations/2026-04-14_wine.qmd",
-    quarto_args = c("--embed-resources"),
+    quarto_args = c("--embed-resources")
   ),
   # Manuscript.
   tar_quarto(
